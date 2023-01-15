aee70c299b2f8b342c9f04cbc45d9bc6b8716679
João da Mata <jlfilho@uea.edu.br>
2023-01-15 03:57:29 -0400

    Ajuste no endpoint categorias/id e produtos/id para retorno de erro HTTP

--- src/main/java/com/jlfilho/cursomc/service/CategoriaService.java
+++ src/main/java/com/jlfilho/cursomc/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jlfilho.cursomc.domain.Categoria;
 import com.jlfilho.cursomc.repositories.CategoriaRepository;
+import com.jlfilho.cursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
--- src/main/java/com/jlfilho/cursomc/service/ProdutoService.java
+++ src/main/java/com/jlfilho/cursomc/service/ProdutoService.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.jlfilho.cursomc.domain.Categoria;
 import com.jlfilho.cursomc.domain.Produto;
 import com.jlfilho.cursomc.repositories.ProdutoRepository;
+import com.jlfilho.cursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class ProdutoService {
@@ -15,9 +17,13 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	public Produto buscar(Integer id) {
+	
+	public Produto find(Integer id) {
 		Optional<Produto> obj = produtoRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
 	}
+	
+
 
 }
